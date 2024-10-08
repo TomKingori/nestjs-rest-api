4de43d2da0080544f9869e14a7e4641473f49229
TomKingori <tomkingori66@gmail.com>
2024-10-08 15:34:16 +0300

    services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,48 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post,
      Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or /users?role=value
   findAll(@Query('role') role?: 'intern' | 'engineer' | 'admin') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() //POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'intern' | 'engineer' | 'admin';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'intern' | 'engineer' | 'admin';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,78 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Tom King",
+            "email": "tomking@gmail.com",
+            "role": "admin"
+        },
+        {
+            "id": 2,
+            "name": "Sandra Manya",
+            "email": "sandra@gmail.com",
+            "role": "engineer"
+        },
+        {
+            "id": 3,
+            "name": "Jane Doe",
+            "email": "janedoe@gmail.com",
+            "role": "intern"
+        },
+        {
+            "id": 4,
+            "name": "John Doe",
+            "email": "johndoe@gmail.com",
+            "role": "intern"
+        },
+        {
+            "id": 5,
+            "name": "Matt Smith",
+            "email": "mattsmith@gmail.com",
+            "role": "engineer"
+        },
+    ]
+
+    findAll(role?: 'intern' | 'engineer' | 'admin'){
+        if(role){
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number){
+        const user = this.users.filter(user => user.id === id)
+        return user
+    }
+
+    create(user: {name: string, email: string, role: 'intern' | 'engineer' | 'admin'}){
+        const usersByHighestId = [...this.users].sort((a,b)=> b.id - a.id)
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updatedUser: {name?: string, email?: string, role?: 'intern' | 'engineer' | 'admin'}){
+        this.users = this.users.map(user=> {
+            if(user.id === id){
+                return {...user, ...updatedUser}
+            }
+            return user
+        })
+
+        return this.findOne(id)
+    }
+
+    delete(id: number){
+        const removedUser = this.findOne(id)
+
+        this.users = this.users.filter(user => user => user.id !== id)
+
+        return removedUser
+    }
+}
